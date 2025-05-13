c4f3d2182ba295789c51bbbc8633705610455344
Eric Welzenbach <e.wbc@gmx.net>
2025-05-13 18:10:39 +0200

    Verbessere die Anzeige von Befehlen in der Funktion run_sudo_command und passe die Berechtigungen für die Shortcut-Pfade an.

--- scripts/overleaf_automated_install.sh
+++ scripts/overleaf_automated_install.sh
@@ -63,7 +63,8 @@ command_exists() {
 # Führt einen Befehl mit sudo aus und prüft auf Erfolg
 run_sudo_command() {
     local cmd="$@"
-    print_status "Führe als root aus: $cmd"
+    local display_cmd=$(echo -e $cmd);
+    print_status "Führe als root aus: $display_cmd"
     # Verwenden Sie eval, um die Befehlszeichenfolge korrekt zu interpretieren
     if ! eval "sudo $cmd"; then
         print_error "Fehler bei der Ausführung von: $cmd"
@@ -326,6 +327,8 @@ ask_and_create_desktop_shortcut() {
             print_status "Desktop-Verknüpfung erfolgreich erstellt: $shortcut_file"
 
             # Speichere den Pfad für die Deinstallation/Verwaltung
+            run_sudo_command "touch $OVERLEAF_SHORTCUT_PATHS_FILE"
+            run_sudo_command "chmod 777 $OVERLEAF_SHORTCUT_PATHS_FILE"
             echo "export OVERLEAF_SHORTCUT_PATH='$shortcut_file'" >> "$OVERLEAF_SHORTCUT_PATHS_FILE"
             echo "export OVERLEAF_ICON_PATH='$win_icon_file'" >> "$OVERLEAF_SHORTCUT_PATHS_FILE"
 
